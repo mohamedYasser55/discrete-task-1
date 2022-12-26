cc7345368ad860d85d02f0d8830509050707f728
mohamedYasser55 <121432150+mohamedYasser55@users.noreply.github.com>
2022-12-26 13:09:08 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, v, j;
+            Console.WriteLine("max:");
+            v = int.Parse(Console.ReadLine());
+            Console.WriteLine("min:");
+            for (i = int.Parse(Console.ReadLine()); i <= v; i++)
+            {
+                for(j =2; j <=i; j++)
+                    if (i % j == 0)
+                    {
+                        break;
+                        if(i == j)
+                        {
+                            Console.WriteLine(i);
+                        }
+                    }
+            }
+
+        }
+    }
+}
